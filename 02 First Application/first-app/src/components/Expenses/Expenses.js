80e46eed77a8443901d4f8f52979e26a2eed0a29
Mati <maticzacza@gmail.com>
2022-11-24 17:15:42 +0200

    chart started

--- 02 First Application/first-app/src/components/Expenses/Expenses.js
+++ 02 First Application/first-app/src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
+import ExpensesList from './ExpensesList';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
@@ -15,28 +15,13 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found!</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => {
-      return (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      );
-    });
-  }
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         onChangeFilter={filterChangeHandler}
         selected={filteredYear}
       ></ExpensesFilter>
-      {expensesContent}
+      <ExpensesList items={filteredExpenses}></ExpensesList>
     </Card>
   );
 }
